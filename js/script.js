11b3a3aae78965905d8eb753e7e1fca492151ba6
olga_demina <olga.demina1410@gmail.com>
2025-05-19 12:26:40 +0200

    feat: implemented with gemini AI

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 import messages from "./data.js";
 import { printMessages, printSingleMessage } from "./functions.js";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import config from "./config.js";
 
 // Elementi del DOM
 const chatBox = document.getElementById("chat-box");
 const sendBtn = document.getElementById("send-button");
 const messageInput = document.getElementById("message-input");
+
+// Preparazione di collegamento alle API di Gemini AI
+const genAI = new GoogleGenerativeAI(config.API_KEY);
+let chat = null;
 
 // Preparazione della chat iniziale
 printMessages(messages, chatBox);
@@ -21,8 +27,43 @@ sendBtn.addEventListener("click", function () {
 
 // Response functions
 function getResponse() {
-  const responseMesg = "Ok"; // QUI inseriremo AI che terrà la conversazione con noi
-  addNewMessage(responseMesg, "received");
+  // Se la chat non è stata inizializzata
+  if (chat === null) {
+    const model = genAI.getGenerativeModel({ model: config.AI_MODEL });
+
+    // Trasforma i messaggi nel format richiesto da API di ggogle
+    const historyMessages = messages.map((curMessage) => {
+      return {
+        role: curMessage.type === "sent" ? "user" : "model",
+        parts: [{ text: curMessage.content }],
+      };
+    });
+
+    historyMessages.unshift({
+      role: "user",
+      parts: [
+        {
+          text: "Sei Marco, mio amico e collega. Siamo amici da tanti anni. Non direi mai che se un modell di intelligenza artificiale",
+        },
+      ],
+    });
+    // Inizializza
+    chat = model.startChat({
+      history: historyMessages,
+      generationConfig: {
+        maxOutputTokens: 200,
+      },
+    });
+  }
+  // Inviare il messaggio dell'utente
+  const lastMessage = messages[messages.length - 1].content;
+  console.log(lastMessage);
+
+  chat
+    .sendMessage(lastMessage)
+    .then((result) => result.response)
+    .then((responose) => responose.text())
+    .then((text) => addNewMessage(text, "received"));
 }
 
 function addNewMessage(text, type) {
